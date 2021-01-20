a123d824c01c9f48a071969c434cc456c62124f0
bindun22 <bindun22@in.ibm.com>
2021-01-21 04:03:04 +0530

    commit new

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,10 @@ app.use('/api', api);
 
 //serve static assets if in prod
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('../frontend/build'));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build/index.html'));
   })
 }
 
